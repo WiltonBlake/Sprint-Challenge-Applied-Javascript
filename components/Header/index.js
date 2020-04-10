57b9f9b9b80ee712a382f4bdd4f952487c1c18ee
Wilton Blake <60114563+wekesalocs@users.noreply.github.com>
2020-04-10 12:06:03 -0400

    Header Done

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+function header() {
+
+    const headerComponent = document.querySelector(".header-container");
+
+    // instantiate elements
+    const head = document.createElement("div");
+    const date = document.createElement("span");
+    const title = document.createElement("h1");
+    const temp = document.createElement("span");    
+
+    // nest elements correctly
+    headerComponent.appendChild(head);
+    head.appendChild(date);
+    head.appendChild(title);
+    head.appendChild(temp);
+
+    // add class names
+    head.classList.add("header");
+    temp.classList.add("temp");
+
+    // set text content
+    date.textContent = "MARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    return head;
+
+}
+
+header();
